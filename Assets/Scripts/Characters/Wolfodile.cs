b923fa2164ecf0ef1a3947d43144dd4a31eba638
Alimen6 <alejoncho98@hotmail.com>
2023-07-19 18:09:56 -0500

    Added wolfodile animation and started work on particle system for sheep death

--- Assets/Scripts/Characters/Wolfodile.cs
+++ Assets/Scripts/Characters/Wolfodile.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Wolfodile : MonoBehaviour
 {
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //this needs to do stuff regarding the sheep count
         Debug.Log("Sheep eaten!");
+        animator.SetTrigger("IsEating");
         Destroy(collision.gameObject);
     }
 }
--- Assets/Scripts/Obstacles/ActivationButton.cs
+++ Assets/Scripts/Obstacles/ActivationButton.cs
@@ -6,13 +6,18 @@ public class ActivationButton : MonoBehaviour
 {
     [SerializeField] GameObject objectToActivate;
 
+    [SerializeField] Sprite pushedButton;
+    [SerializeField] Sprite releasedButton;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         objectToActivate.GetComponent<IActivatable>().Activate();
+        GetComponentInChildren<SpriteRenderer>().sprite = pushedButton;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         objectToActivate.GetComponent<IActivatable>().Deactivate();
+        GetComponentInChildren<SpriteRenderer>().sprite = releasedButton;
     }
 }
