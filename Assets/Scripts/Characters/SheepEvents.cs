dffb331f5357f1e1be118ac769fefed5453ce41d
Alimen6 <alejoncho98@hotmail.com>
2023-07-20 19:05:27 -0500

    Created death vfx for npsheep

--- Assets/Scripts/Characters/SheepEvents.cs
+++ Assets/Scripts/Characters/SheepEvents.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class SheepEvents : MonoBehaviour
 {
     [SerializeField] GameEvent OnSheepDied;
+    [SerializeField] GameObject deathParticleEffects;
+    [SerializeField] GameObject deathGhost;
 
     private void OnDestroy()
     {
+        Instantiate(deathParticleEffects, transform.position, Quaternion.identity);
+        Instantiate(deathGhost, transform.position, Quaternion.identity);
         OnSheepDied?.Raise();
     }
 }
