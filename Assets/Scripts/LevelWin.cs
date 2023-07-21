13a55668ce5ab00dfedd4fb4cc65414321ea4525
Wispre <33339713+Wispre@users.noreply.github.com>
2023-07-21 12:38:13 -0700

    Added stars to UI

--- Assets/Scripts/LevelWin.cs
+++ Assets/Scripts/LevelWin.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelWin : MonoBehaviour
 {
+    public List<Image> Sprites = new List<Image>();
+    public List<Sprite> WinSprites = new List<Sprite>();
+
     private TransitionChange transition;
+    private WaitForSeconds delayPerStar= new WaitForSeconds(1f);
+    private int sheepLost;
 
     private void Awake()
     {
@@ -23,5 +29,23 @@ public class LevelWin : MonoBehaviour
         {
             transition.LoadLevel("LevelSelect");
         }
+    }
+
+    public void SetScore(int amountDied)
+    {
+        this.sheepLost = amountDied;
+    }
+
+    private IEnumerator ShowScore()
+    {
+        yield return delayPerStar;
+
+        for (int i = 0; i < sheepLost; i++)
+        {
+            Sprites[i].sprite = WinSprites[i];
+            yield return delayPerStar;
+        }
+
+
     }
 }
