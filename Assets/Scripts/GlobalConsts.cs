f6ce24d01986292fb582107585fac4a14e3f4a47
Wispre <33339713+Wispre@users.noreply.github.com>
2023-07-21 15:53:27 -0700

    Conflict check

--- Assets/Scripts/GlobalConsts.cs
+++ Assets/Scripts/GlobalConsts.cs
@@ -2,4 +2,5 @@ public static class GlobalConsts
 {
     public const int SCENE_COUNT = 6;
     public const int MAX_SHEEP_DEATHS = 3;
+    public const string LEVELS_UNLOCKED = "Levels_Unlocked";
 }
